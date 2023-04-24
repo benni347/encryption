019467875669b903f751e1cc78ed88320b3d7189
Cedric Skwar <cdrc@skwar.me>
2023-04-24 11:18:04 +0200

    added test cases for kyber encrypt and decrypt.

--- kyber_test.go
+++ kyber_test.go
@@ -15,3 +15,32 @@ func TestGenerateKyberKeyPair(t *testing.T) {
 		t.Errorf("Public key has incorrect length: got %d, want %d", len(publicKey), kyberk2so.Kyber1024PKBytes)
 	}
 }
+
+func TestEncryptKyber(t *testing.T) {
+	_, publicKey, _ := GenerateKyberKeyPair()
+	ciphertext, sharedSecret, err := EncryptKyber(publicKey)
+	if err != nil {
+		t.Errorf("Error encrypting with Kyber: %v", err)
+	}
+
+	if len(ciphertext) != kyberk2so.Kyber1024CTBytes {
+		t.Errorf("Ciphertext has incorrect length: got %d, want %d", len(ciphertext), kyberk2so.Kyber1024CTBytes)
+	}
+	if len(sharedSecret) != kyberk2so.KyberSSBytes {
+		t.Errorf("Shared secret has incorrect length: got %d, want %d", len(sharedSecret), kyberk2so.KyberSSBytes)
+	}
+}
+
+func TestDecryptKyber(t *testing.T) {
+	_, publicKey, _ := GenerateKyberKeyPair()
+	ciphertext, _, _ := EncryptKyber(publicKey)
+	privateKey, _, _ := GenerateKyberKeyPair()
+	sharedSecret, err := DecryptKyber(ciphertext, privateKey)
+	if err != nil {
+		t.Errorf("Error decrypting with Kyber: %v", err)
+	}
+
+	if len(sharedSecret) != kyberk2so.KyberSSBytes {
+		t.Errorf("Shared secret has incorrect length: got %d, want %d", len(sharedSecret), kyberk2so.KyberSSBytes)
+	}
+}
